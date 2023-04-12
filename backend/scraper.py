76063a16573898c3eee33e31a14826209a3dfd1b
Igor Brizack <igor_brizack@hotmail.com>
2023-04-12 16:43:43 -0300

    feat: buscape product scrape done

--- backend/scraper.py
+++ backend/scraper.py
@@ -6,19 +6,6 @@ import time
 # https://lista.mercadolivre.com.br/geladeira#D[A:geladeira]
 # https://www.buscape.com.br/search?q=geladeira
 
-next_page_class_meli = "andes-pagination__link shops__pagination-link ui-search-link"
-
-def scrape_next_page_link(html_content, web_site):
-    bs_select_next = _bs4.BeautifulSoup(html_content, "html.parser")
-
-    if web_site == 'mercadolivre':
-        try:
-            result = bs_select_next.find_all("a", class_= next_page_class_meli)
-            print(str(result[0]["href"]))
-            return str(result[0]["href"])
-        except Exception:
-            None
-
 
 def fetch(url: str):
     try:
@@ -31,7 +18,7 @@ def fetch(url: str):
 
 
 def generate_url(web_site: str, product: str):
-    if web_site == 'mercadolivre':
+    if web_site == "mercadolivre":
         url = f"https://lista.mercadolivre.com.br/{product}#D[A:{product}]"
         return url
     else:
@@ -39,31 +26,86 @@ def generate_url(web_site: str, product: str):
         return url
 
 
-def meli_qtd_pages(data):
-    content = _bs4.BeautifulSoup(data, "html.parser")
-    result = content.find("li", class_= 'andes-pagination__page-count')
-    return str(result.contents[2])
+def scrape_meli_products(url):
+    html_content = fetch(url)
+    content_parser = _bs4.BeautifulSoup(html_content, "html.parser")
+    result = content_parser.find_all(
+        "li", class_="ui-search-layout__item shops__layout-item"
+    )
+
+    list_of_products_data = []
+
+    for product in result:
+        product_dict = {
+            "web_site": "Mercado Livre",
+            "descriptions": str(
+                product.find(
+                    "h2", class_="ui-search-item__title shops__item-title"
+                ).text
+            ),
+            "image_link": str(
+                product.div.div.div.a.div.div.div.div.div.img["data-src"]
+            ),
+            "price": 'R$ ' + (
+                ( 
+                    str(
+                        product.find(
+                            "span",
+                            class_="price-tag ui-search-price__part shops__price-part",
+                        ).text,
+                    )
+                ).split(" ")
+            )[0],
+            "external_link": str((product).find("a", class_="ui-search-link")["href"]),
+        }
+
+        list_of_products_data.append(product_dict)
+
+    return list_of_products_data
 
 
-def scrape_meli_content(html_content, url_list):
-    # print('cheguei aqui')
-    
-    print(len(url_list))
-    new_page_url = scrape_next_page_link(html_content, 'mercadolivre')
-    new_url = fetch(new_page_url)
-    if new_page_url:
-        url_list.append(new_page_url)
-        scrape_meli_content(new_url, url_list)
+def scrape_buscape_products(url):
+    class_text = 'Paper_Paper__HIHv0 Paper_Paper__bordered__iuU_5 Card_Card__LsorJ Card_Card__clicable__5y__P SearchCard_ProductCard__1D3ve'
+    html_content = fetch(url)
+    content_parser = _bs4.BeautifulSoup(html_content, "html.parser")
+    result = content_parser.find_all("div", class_={class_text})
 
-        
-    
+    # print(result[0].find("h2", {"data-testid": "product-card::name"}).text)
+    # print(result[0].find("img", {"data-nimg": "fill"})["src"])
+    # print(result[0].find("p", {"data-testid": "product-card::price"}).text)
+    # print(result[0].find("a", {"data-testid": "product-card::card"})["href"])
+    # print(result)
+
+    list_of_products_data = []
+
+    for product in result:
+        product_dict = {
+            "web_site": "Busca Pé",
+            "descriptions": str(
+                product.find("h2", {"data-testid": "product-card::name"}).text
+            ),
+            "image_link": str(product.find("img", {"data-nimg": "fill"})["src"]),
+            "price": str(
+                product.find("p", {"data-testid": "product-card::price"}).text
+            ),
+            "external_link": str(
+                product.find("a", {"data-testid": "product-card::card"})["href"]
+            ),
+        }
+
+        list_of_products_data.append(product_dict)
+
+    return list_of_products_data
+
+
 def manage_scrape(web_site: str, product: str):
     main_url = generate_url(web_site, product)
-    main_url_fetch = fetch(main_url)
-    if web_site == 'mercadolivre':
-        # total_pages = meli_qtd_pages(main_url_fetch)
-        return scrape_meli_content(main_url_fetch, [])
+    if web_site == "mercadolivre":
+        print(scrape_meli_products(main_url))
+        return
+    else:
+        print(scrape_buscape_products(main_url))
+        return
 
-        
 
-manage_scrape('mercadolivre', 'geladeira')
+manage_scrape("buscape", "geladeira")
