9250eac4b3b57f3135a2e36060d4c2179578cdec
Igor Brizack <igor_brizack@hotmail.com>
2023-04-13 13:52:59 -0300

    feat: frontend / dockerfile

--- backend/server/database.py
+++ backend/server/database.py
@@ -3,56 +3,18 @@ import motor.motor_asyncio
 
 from scraper import manage_scrape
 
-from bson.objectid import ObjectId
-
-from fastapi.encoders import jsonable_encoder
-
-MONGO_DETAILS = "mongodb://localhost:27017"
+MONGO_DETAILS = "mongodb://crawler_web_db:27017"
 
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
 
-database = client.students
 database_products = client.products
 
-students_collection = database.get_collection("students_collection")
 products_collection = database_products.get_collection("products_collection")
 
 website_helper = {
         "mercadolivre": 'Mercado Livre',
         "buscape": 'Busca Pé'
     }
-
-# async def product_helper(products) -> 'list[dict]':
-#     products_list = []
-
-#     for product in await products.to_list(length=1000):
-#         products_list.append({
-#             "id": str(product["_id"]),
-#             "product_type": str(product["product_type"]),
-#             "description": product["description"],
-#             "website": product["website"],
-#             "external_link": product["external_link"],
-#             "image_link": product['igame_link']
-#         })
-
-#     content = { 'products' : products_list}
-#     print(content)
-#     return content
-
-def student_helper(student) -> dict:
-    return {
-        "id": str(student["_id"]),
-        "fullname": student["fullname"],
-        "email": student["email"]
-    }
-
-# Retrieve all students present in the database
-async def retrieve_students():
-    students = []
-    async for student in students_collection.find():
-        student.append(student_helper(student))
-    return students
-
 
 async def add_product(website: str, product: str):
     scrape_data = manage_scrape(website, product)
@@ -78,14 +40,15 @@ async def add_product(website: str, product: str):
 async def get_products(website: str, product: str):
     products_data = products_collection.find({"website": website_helper[website], "product_type": product})
 
-    if not products_data:
-        data = add_product(website, product)
+    products_data_result = await products_data.to_list(length=1000)
+
+    if not products_data_result:
+        data = await add_product(website, product)
         return data
     
-    print('chegando aqui')
     products_list = []
 
-    for prod in await products_data.to_list(length=1000):
+    for prod in products_data_result:
         products_list.append({
             "id": str(prod["_id"]),
             "product_type": str(prod["product_type"]),
@@ -96,37 +59,3 @@ async def get_products(website: str, product: str):
         })
 
     return products_list
-
-
-# Add a new student into to the database
-async def add_student(student_data: dict) -> dict:
-    student = await students_collection.insert_one(student_data)
-    new_student = await students_collection.find_one({"_id": student.inserted_id})
-    return student_helper(new_student)
-
-# Retrieve a student with a matching ID
-async def retrieve_student(id: str) -> dict:
-    student = await students_collection.find_one({"_id": ObjectId(id)})
-    if student:
-        return student_helper(student)
-    
-# Update a student with a matching ID
-async def update_student(id: str, data: dict):
-    #return false if an empty request body is sent.
-    if len(data) < 1:
-        return False
-    stundent = await students_collection.find_one({"_id": ObjectId(id)})
-    if stundent:
-        update_student = await students_collection.update_one(
-            {"_id": ObjectId(id)}, {"$set": data}
-        )
-        if update_student:
-            return True
-        return False
-
-# Delete a student from the database
-async def delete_student(id: str):
-    student = await students_collection.find_one({"_id": ObjectId(id)})
-    if student:
-        await students_collection.delete_one({"_id": ObjectId(id)})
-        return True
